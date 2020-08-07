49f919be136b6a75cf182193f8feb51a8d015fbc
AntoineGS <antoineg.simard@gmail.com>
2020-08-07 16:31:16 -0400

    Finished chapter 10

--- app/__init__.py
+++ app/__init__.py
@@ -3,30 +3,45 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-import logging, os
+import logging
+import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask_mail import Mail
 
+config = Config
 app = Flask(__name__)
-app.config.from_object(Config)
+# Flask configs
+app.config['SECRET_KEY'] = config.secretKey
+# SQLAlchemy configs
+app.config['SQLALCHEMY_DATABASE_URI'] = config.sqlAlchemyDatabaseUri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Flask Mail configs
+app.config['MAIL_SERVER'] = config.mailServer
+app.config['MAIL_PORT'] = config.mailPort
+app.config['MAIL_USE_TLS'] = config.mailUseTls
+app.config['MAIL_USERNAME'] = config.mailUsername
+app.config['MAIL_PASSWORD'] = config.mailPassword
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
 if not app.debug:
-    if app.config['MAIL_SERVER']:
+    if config.mailServer:
         auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        if config.mailUsername or config.mailPassword:
+            auth = (config.mailUsername, config.mailPassword)
         secure = None
-        if app.config['MAIL_USE_TLS']:
+        if config.mailUseTls:
             secure = ()
         mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            mailhost=(config.mailServer, config.mailPort),
+            fromaddr='no-reply@' + config.mailServer,
+            toaddrs=config.admins, subject='Microblog Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
@@ -42,4 +57,3 @@ if not app.debug:
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
-
--- config.py
+++ config.py
@@ -2,16 +2,15 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+# class Config(object):
+class Config:
+    secretKey = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    sqlAlchemyDatabaseUri = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['antoineg.simard@gmail.com']
-    POSTS_PER_PAGE = 25
-
+    mailServer = os.environ.get('MAIL_SERVER')
+    mailPort = int(os.environ.get('MAIL_PORT') or 25)
+    mailUseTls = os.environ.get('MAIL_USE_TLS') is not None
+    mailUsername = os.environ.get('MAIL_USERNAME')
+    mailPassword = os.environ.get('MAIL_PASSWORD')
+    admins = ['antoineg.simard@gmail.com']
+    postsPerPage = 25
